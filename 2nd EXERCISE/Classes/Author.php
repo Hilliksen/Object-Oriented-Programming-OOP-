612080fd509e366a940673f17dcc2d44515e80f2
Anzor <soldaev.anzor@gmail.com>
2024-12-09 14:00:58 +0100

    09/12/2024 AFTERNOON SESSION START

--- 2nd EXERCISE/Classes/Author.php
+++ 2nd EXERCISE/Classes/Author.php
@@ -6,7 +6,7 @@ class Author{
 
     private string $lastName;
 
-    private string $birthDay;
+    private DateTime $birthDay;
 
     private string $birthPlace;
 
@@ -17,8 +17,8 @@ class Author{
     public function __construct(string $name, string $lastName, string $birthDay, string $birthPlace) {
         $this -> name = $name;
         $this -> lastName = $lastName;
-        $this -> birthDay = new DateTime ($birthDay);
-        $this -> birthPlace = $birthPlace
+        $this -> birthDay = new DateTime($birthDay);
+        $this -> birthPlace = $birthPlace;
         $this -> books = [];
     }
 
@@ -91,16 +91,22 @@ class Author{
 
 
 
-    
-    //IMPO FUNCTIONS 
 
-    public function getInfos(){
-        return $this ->name. " " . $this -> lastName . "is an american author born in " . $this -> getBirthDay() -> format("d/m/Y"). " who wrote well known books like : <br>" . 
-    }
+    //IMPO FUNCTIONS 
 
     public function addBook(Book $book){
         $this-> books[] = $book;
     }
+    public function getInfos(){
+        $result = $this ->name. " " . $this -> lastName . " is an american author born in " . $this -> getBirthDay() -> format("d/m/Y"). " who wrote well known books like : <br>" ;
+
+        foreach($this -> books as $book ){
+             
+            $result .= $book -> getTitle(). "( made in ". $dateCreation = getDateCreation() -> format ("d/m/Y"). ") and containing " . getPageNumber(). " costs " . getPrice() ."euros <br>";
+
+        }
+    }
+
 
    
 }
--- 2nd EXERCISE/index.php
+++ 2nd EXERCISE/index.php
@@ -7,7 +7,16 @@ spl_autoload_register(function($class_name) {
 
     require 'classes/'  . $class_name . '.php';
     //! for explanation look at index.php in 1st EXERCISE
-})
+});
 
 
-$Stephen= new Author ("Stephen","King","21-09-1947","Portland USA",)
+$Stephen = new Author ("Stephen","King","21-09-1947","Portland USA",);
+
+$It = new Book ("IT","1986","1138","20");
+$Sematary = new Book ("Sematary","1983","374","15");
+$TheStand= new Book ("The Stand","1978","823","14");
+$TheShinning = new Book ("The Shinning","1977","447","16");
+
+
+echo $Stephen -> getInfos();
+
